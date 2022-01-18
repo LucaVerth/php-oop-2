7462034c55172361644f89af63998ada70816494
LucaVerth <luca.verthuy@gmail.com>
2022-01-18 14:10:22 +0100

    First Commit init

---
+++ classes/Products.php
@@ -0,0 +1,13 @@
+<?php 
+
+class Product{
+
+  private $name;
+  private $description;
+  private $status;
+  private $price;
+  private $discount;
+
+  
+}
+
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+require_once __DIR__ . "/classes/Products.php";
+require_once __DIR__ . "/classes/User.php";
+
+$new_product = new Product();
+var_dump($new_product);
+
+$new_user = new User();
+var_dump($new_user);
+
+
+?>
